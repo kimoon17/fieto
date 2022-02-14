bd3404d49640f1ba3ae54b38a28be9cae7f57ddd
kimon <kimon@kimons-MacBook-Pro.local>
2022-02-14 09:57:27 -0800

    added styles

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,22 @@
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import './styles.css';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    signupWrapper: {
+        height: '800px',
+        width: '400px',
+        padding: '20px'
+    },
+    signupHeading: {
+        marginBottom: '30px'
+    }
+}))
 
 const SignUp = () => {
+    const classes = useStyles();
     return (
-        <div className="signup-wrapper">
-            <h1>Sign Up</h1>
+        <div className={classes.signupWrapper}>
+            <h1 className={classes.signupHeading}>Sign Up</h1>
             <Form>
                 <FormGroup>
                     <Input type="email" name="email" id="exampleEmail" placeholder="Username" />
